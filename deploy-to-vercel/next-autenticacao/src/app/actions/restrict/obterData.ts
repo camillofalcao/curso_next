26918782726b5adc8644f653fb57b86db36fb1a8
Camillo <camillofalcao@gmail.com>
2025-10-26 09:54:56 -0300

    Eliminando arquivos enviados desnecessariamente

--- deploy-to-vercel/next-autenticacao/src/app/actions/restrict/obterData.ts
+++
@@ -1,8 +0,0 @@
-'use server'
-
-import { requireUser } from '../requireUser';
-
-export default async function obterData(): Promise<Date> {
-  await requireUser();
-  return Promise.resolve(new Date());
-}
